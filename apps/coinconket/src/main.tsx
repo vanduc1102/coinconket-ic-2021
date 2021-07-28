527e61b4a356cb1ec3574b1729aa3c68f92ef146
DUC NGUYEN <vanducld@gmail.com>
2021-07-29 00:25:40 +0700

    add Material-UI

--- apps/coinconket/src/main.tsx
+++ apps/coinconket/src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
 import App from './app/app';
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
   document.getElementById('root')
 );
